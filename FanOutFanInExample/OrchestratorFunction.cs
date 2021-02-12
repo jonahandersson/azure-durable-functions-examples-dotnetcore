c99b9fd232513b5ab8655880360bba78e378a436
Jonah Andersson <cjonah@hotmail.se>
2021-02-12 20:25:32 +0100

    Initial commit with initial project. To add more examples for different patterns

---
+++ FanOutFanInExample/OrchestratorFunction.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace FanOutFanInExample
+{
+    public static class OrchestratorFunction
+    {
+        [FunctionName("OrchestratorFunction")]
+        public static async Task<string> RunOrchestrator(
+            [OrchestrationTrigger] IDurableOrchestrationContext context)
+        {
+            var orders = context.GetInput<List<string>>();
+            var orderTasks = new List<Task<double>>();
+            for (int i = 0; i < orders.Count; i++)
+            {
+                var orderTaskToDo = context.CallActivityAsync<double>("CalculateOrderActivity",
+                    orders[i]);
+                orderTasks.Add(orderTaskToDo);
+            }
+
+            var orderAmounts = await Task.WhenAll(orderTasks);
+
+            var invoiceOrderSummary = await context.CallActivityAsync<string>("GenerateOrderInvoiceActivity",
+                  orderAmounts);
+
+            return invoiceOrderSummary;
+        }
+
+
+    }
+}
---
+++ FanOutFanInExample/StarterFunction.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace FanOutFanInExample
+{
+    public static class StarterFunction
+    {
+        [FunctionName("StarterFunction")]
+        public static async Task<HttpResponseMessage> HttpStart(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
+            [DurableClient] IDurableOrchestrationClient starter,  ILogger log)
+        {
+            var orderIds = Enumerable.Range(0, 10);
+
+            // Function input comes from the request content.
+            string instanceId = await starter.StartNewAsync("Orchestrator", orderIds.ToList());
+            log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+
+            return starter.CreateCheckStatusResponse(req, instanceId);
+        }
+    }
+}
